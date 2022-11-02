c62cfa8b3258033bc996d1db57317f822e097387
matthew <matthewgoodbar@gmail.com>
2022-11-02 11:49:09 -0700

    Create class QuestionLike, various instance methods for ORM classes

--- questions_database.rb
+++ questions_database.rb
@@ -66,6 +66,22 @@ class Question
         @body = options['body']
     end
 
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
+        data.map {|datum| Question.new(datum)}
+    end
+
+    def self.find_by_id(id)
+        question =
+        QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT * FROM questions
+        WHERE id = ?
+        SQL
+
+        return nil unless question.length > 0
+        Question.new(question.first)
+    end
+
     def self.find_by_author_id(user_id)
         question = QuestionsDatabase.instance.execute(<<-SQL,user_id)
         SELECT * FROM questions
@@ -83,10 +99,6 @@ class Question
         Reply.find_by_question_id(@id)
     end
 
-    def self.all
-        data = QuestionsDatabase.instance.execute('SELECT * FROM questions')
-        data.map {|datum| Question.new(datum)}
-    end
 
 end
 
@@ -99,6 +111,22 @@ class Reply
         @question_id = options['question_id']
         @parent_id = options['parent_id']
         @body = options['body']
+    end
+
+    def self.all
+        data = QuestionsDatabase.instance.execute('SELECT * FROM replies')
+        data.map {|datum| Reply.new(datum)}
+    end
+
+    def self.find_by_id(id)
+        reply =
+        QuestionsDatabase.instance.execute(<<-SQL, id)
+        SELECT * FROM replies
+        WHERE id = ?
+        SQL
+
+        return nil unless reply.length > 0
+        Reply.new(reply.first)
     end
 
     def self.find_by_user_id(user_id)
@@ -121,11 +149,71 @@ class Reply
         reply.map {|datum| Reply.new(datum)}
     end
 
-    def self.all
-        data = QuestionsDatabase.instance.execute('SELECT * FROM replies')
-        data.map {|datum| Reply.new(datum)}
+    def author
+        User.find_by_id(@user_id)
+    end
+    
+    def question
+        Question.find_by_id(@question_id)
     end
 
+    def parent_reply
+        return nil if @parent_id == nil
+        Reply.find_by_id(@parent_id)
+    end
+
+    def child_replies
+        replies = QuestionsDatabase.instance.execute(<<-SQL, @id)
+        SELECT * FROM replies
+        WHERE parent_id = ?
+        SQL
+
+        return nil unless replies.length > 0
+        replies.map {|datum| Reply.new(datum)}
+    end
+
+end
+
+class QuestionFollow
+    attr_accessor :user_id, :question_id
+    attr_reader :id
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
+
+    def self.followers_for_question_id(question_id)
+        follows = QuestionsDatabase.instance.execute(<<-SQL, question_id)
+        SELECT * FROM question_follows f
+        JOIN users u ON f.user_id = u.id
+        WHERE f.question_id = ?
+        SQL
+
+        return nil unless follows.length > 0
+        follows.map {|datum| User.new(datum)}
+    end
+
+    def self.followed_questions_for_user_id(user_id)
+        follows = QuestionsDatabase.instance.execute(<<-SQL, user_id)
+        SELECT * FROM question_follows f
+        JOIN questions q ON f.question_id = q.id
+        WHERE f.user_id = ?
+        SQL
+
+        return nil unless follows.length > 0
+        follows.map {|datum| Question.new(datum)}
+    end
+end
+
+class QuestionLike
+    attr_accessor :user_id, :question_id
+    attr_reader :id
+    def initialize(options)
+        @id = options['id']
+        @user_id = options['user_id']
+        @question_id = options['question_id']
+    end
 end
 
 # authored_questions (call on a user intance)
@@ -137,12 +225,22 @@ end
 
 # replys = Reply.all
 # replys.each do |reply|
-#     # p question
+#     p reply.author
 
 # end
 
 # replies = Reply.all
 # replies.each do |reply|
-#     p reply.authored_questions
+#     p reply.parent_reply
 # end
+
+
+#TESTS
+#Reply#parent_reply #No child replies in DB right now
+#Reply#child_replies
+
+#QuestionFollow::followers_for_question_id
+#QuestionFollow::followed_questions_for_user_id
+
+p QuestionFollow.followed_questions_for_user_id(3)
 
